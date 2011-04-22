a9b68fdd18e9ad6e211243580eac45dc44a347bf
psyomn <psyomn@xenagoras.net>
2011-04-22 02:05:48 -0400

    Main loop for game playing is functional.

--- main.cxx
+++ main.cxx
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include <limits>
 #include <ctime>
 #include <cstdlib>
@@ -8,23 +9,45 @@ using namespace std;
 #include "Ren.hxx"
 
 int main(){
-  Board b;
-  Ren r;
-  unsigned int px;
-  unsigned int py;
+  Board *b;
+  
+  unsigned int px; // pick x - also used for width initialization
+  unsigned int py; // pick y - also used for height initialization
+  unsigned int nb; // num of bombs
+  bool newgame = true;
+  string str;
 
-  while(b.getState()){
-    cout << " - Private view - " << endl;
-    b.print();
-    cout << " - Game view - " << endl;
-    b.printGame();
-	cout << "X coordinate: ";
+  cout << "Welcome to sweepminer!" << endl;
+
+  while(newgame){
+    cout << "Enter board width  : ";
 	cin >> px;
-	cout << "Y coordinate: ";
+	cout << "Enter board height : ";
 	cin >> py;
-	b.pick(px, py);
-  }
+	cout << "Number of bombs    : ";
+	cin >> nb;
+	b = new Board(px, py, nb);
+	
+	while(b->getState()){
+      cout << " - Private view - " << endl;
+      b->print();
+      cout << " - Game view - " << endl;
+      b->printGame();
+      
+	  cout << "X coordinate: ";
+      cin >> px;
+      cout << "Y coordinate: ";
+      cin >> py;
+      
+	  b->pick(px, py);
+    }
+    delete b; 
 
+	cout << "Play again ? [yes/no] : ";
+	cin >> str;
+	
+	str == "yes" ? newgame=true : newgame=false;
+  }
 /*
   while(b.getState()) <- while game is ok.
 
